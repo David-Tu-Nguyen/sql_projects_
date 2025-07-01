843208710d3ce8167873bf4d8c3a366e9fefb117
David Tu Nguyen <nguyenminhtumo@gmail.com>
2025-07-01 17:14:48 +1000

    Update Quality Check of Silver Table.sql

--- sql-data-warehouse-project/scripts/silver/Quality Check of Silver Table.sql
+++ sql-data-warehouse-project/scripts/silver/Quality Check of Silver Table.sql
@@ -1,3 +1,35 @@
+/*
+===============================================================================
+Quality Check of Bronze Table: crm_cust_info
+===============================================================================
+*/
+-- Check For Nulls or Duplicates in Primary 
+-- Expectation: No Result
+
+SELECT
+cst_id,
+COUNT(*)
+FROM bronze.crm_cust_info
+GROUP BY cst_id
+HAVING COUNT(*) > 1 OR cst_id IS NULL 
+
+--Check for unwanted Spaces 
+-- Expectation: No Results 
+SELECT cst_firstname 
+FROM bronze.crm_cust_info
+WHERE cst_firstname != TRIM(cst_firstname)
+
+
+--Data Standardization & Consistency
+SELECT DISTINCT cst_gndr
+FROM bronze.crm_cust_info
+  
+/*
+===============================================================================
+Quality Check of Silver Table: crm_cust_info
+===============================================================================
+*/
+
 -- Check For Nulls or Duplicates in Primary 
 -- Expectation: No Result
 
@@ -20,3 +52,9 @@ SELECT DISTINCT cst_gndr
 FROM silver.crm_cust_info
 
 SELECT * FROM silver.crm_cust_info
+
+/*
+===============================================================================
+Quality Check of Bronze Table: crm_cust_info
+===============================================================================
+*/
