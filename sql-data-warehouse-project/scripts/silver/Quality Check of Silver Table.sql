836d0a1ef51e1242c175dd0cfd11b58a48eef170
David Tu Nguyen <nguyenminhtumo@gmail.com>
2025-07-02 09:32:08 +1000

    Update Quality Check of Silver Table.sql

--- sql-data-warehouse-project/scripts/silver/Quality Check of Silver Table.sql
+++ sql-data-warehouse-project/scripts/silver/Quality Check of Silver Table.sql
@@ -55,6 +55,73 @@ SELECT * FROM silver.crm_cust_info
 
 /*
 ===============================================================================
-Quality Check of Bronze Table: crm_cust_info
+Quality Check of Bronze Table: crm_prd_info
 ===============================================================================
 */
+-- Check For Nulls or Duplicates in Primary 
+-- Expectation: No Result
+
+SELECT
+prd_id,
+COUNT(*)
+FROM bronze.crm_prd_info
+GROUP BY prd_id
+HAVING COUNT(*) > 1 OR prd_id IS NULL 
+
+--Check for unwanted Spaces 
+-- Expectation: No Results 
+SELECT prd_nm 
+FROM bronze.crm_prd_info
+WHERE prd_nm  != TRIM(prd_nm )
+
+
+--Data Standardization & Consistency
+SELECT prd_cost
+FROM bronze.crm_prd_info
+WHERE prd_cost < 0 OR prd_cost IS NULL
+
+SELECT DISTINCT prd_line
+FROM bronze.crm_prd_info
+
+--Check for Invalid Date Orders
+SELECT * 
+FROM bronze.crm_prd_info
+WHERE prd_end_dt < prd_start_dt
+
+/*
+===============================================================================
+Quality Check of Silver Table: crm_prd_info
+===============================================================================
+*/
+-- Check For Nulls or Duplicates in Primary 
+-- Expectation: No Result
+
+SELECT
+prd_id,
+COUNT(*)
+FROM silver.crm_prd_info
+GROUP BY prd_id
+HAVING COUNT(*) > 1 OR prd_id IS NULL 
+
+--Check for unwanted Spaces 
+-- Expectation: No Results 
+SELECT prd_nm 
+FROM silver.crm_prd_info
+WHERE prd_nm  != TRIM(prd_nm )
+
+
+--Data Standardization & Consistency
+SELECT prd_cost
+FROM silver.crm_prd_info
+WHERE prd_cost < 0 OR prd_cost IS NULL
+
+SELECT DISTINCT prd_line
+FROM silver.crm_prd_info
+
+--Check for Invalid Date Orders
+SELECT * 
+FROM silver.crm_prd_info
+WHERE prd_end_dt < prd_start_dt
+
+SELECT * 
+FROM silver.crm_prd_info
