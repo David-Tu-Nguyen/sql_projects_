6c0b83bca70c1fcae5c7ab5b0b467a91efb1c10e
David Tu Nguyen <nguyenminhtumo@gmail.com>
2025-07-03 21:31:22 +1000

    Update Quality Check of Silver Table.sql

--- sql-data-warehouse-project/scripts/silver/Quality Check of Silver Table.sql
+++ sql-data-warehouse-project/scripts/silver/Quality Check of Silver Table.sql
@@ -127,3 +127,79 @@ WHERE prd_end_dt < prd_start_dt
 
 SELECT * 
 FROM silver.crm_prd_info
+
+/*
+===============================================================================
+Quality Check of Silver Table: crm_sales_details
+===============================================================================
+*/
+
+--Check for unwanted Spaces 
+-- Expectation: No Results 
+SELECT sls_ord_num
+FROM silver.crm_sales_details
+WHERE sls_ord_num  != TRIM(sls_ord_num)
+
+--Check for connected
+-- Expectation: No Results 
+SELECT sls_prd_key 
+FROM silver.crm_sales_details
+WHERE sls_prd_key  NOT IN (SELECT prd_key FROM silver.crm_prd_info)
+
+SELECT  sls_cust_id
+FROM silver.crm_sales_details
+WHERE  sls_cust_id  NOT IN (SELECT cst_id FROM silver.crm_cust_info)
+
+--Check for Invalid Dates 
+SELECT 
+NULLIF(sls_order_dt,0) sls_order_dt 
+FROM bronze.crm_sales_details
+WHERE sls_order_dt <= 0 -- Negative numbers or zeros can not be cast to a date 
+OR LEN(sls_order_dt) != 8 
+OR sls_order_dt > 20500101
+OR sls_order_dt < 19000101
+
+SELECT 
+NULLIF(sls_ship_dt,0) sls_ship_dt 
+FROM bronze.crm_sales_details
+WHERE sls_ship_dt <= 0 -- Negative numbers or zeros can not be cast to a date 
+OR LEN(sls_ship_dt) != 8 
+OR sls_ship_dt > 20500101
+OR sls_ship_dt < 19000101
+
+SELECT 
+NULLIF(sls_due_dt,0)sls_due_dt
+FROM bronze.crm_sales_details
+WHERE sls_due_dt <= 0 -- Negative numbers or zeros can not be cast to a date 
+OR LEN(sls_due_dt) != 8 
+OR sls_due_dt > 20500101
+OR sls_due_dt < 19000101
+
+-- Check for Invalid Date Orders
+SELECT * 
+FROM silver.crm_sales_details
+WHERE sls_order_dt > sls_ship_dt OR sls_order_dt > sls_due_dt
+
+-- Check Data Consistency: Between Sales, Quantity, and Price 
+-- >> Sales = Quantity * Price
+-- >> Values must not be NULL, zero, or negative.
+
+SELECT DISTINCT 
+sls_sales,
+sls_quantity,
+sls_price 
+FROM silver.crm_sales_details
+WHERE sls_sales != sls_quantity * sls_price 
+OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL 
+OR sls_sales <= 0 OR sls_quantity <= 0 OR sls_price <= 0
+ORDER BY sls_sales,
+sls_quantity,
+sls_price 
+
+
+
+
+
+
+
+
