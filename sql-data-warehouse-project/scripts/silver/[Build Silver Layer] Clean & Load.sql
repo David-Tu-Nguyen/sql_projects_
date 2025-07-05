c45fb89b525809556bc6c5a75234b15b8f0ea09e
David Tu Nguyen <nguyenminhtumo@gmail.com>
2025-07-05 13:10:24 +1000

    Update [Build Silver Layer] Clean & Load.sql

--- sql-data-warehouse-project/scripts/silver/[Build Silver Layer] Clean & Load.sql
+++ sql-data-warehouse-project/scripts/silver/[Build Silver Layer] Clean & Load.sql
@@ -125,6 +125,19 @@ CASE
 	ELSE 'n/a'
 END AS gen -- Normalize gender values and handle unknown cases 
 FROM bronze.erp_cust_az12
- 
+
+-- [Build Silver Layer] Clean & Load erp_loc_a101
+INSERT INTO silver.erp_loc_a101 (cid, cntry)
+
+SELECT 
+REPLACE (cid, '-', '') cid,
+CASE 
+	WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+	WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
+	WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
+	ELSE TRIM(cntry)
+END AS cntry 
+FROM bronze.erp_loc_a101
+
 
 
