a3e9e0ac1832e0c2bb4e3d94820bd30e4de924bd
David Tu Nguyen <nguyenminhtumo@gmail.com>
2025-07-05 12:41:35 +1000

    Update [Build Silver Layer] Clean & Load.sql

--- sql-data-warehouse-project/scripts/silver/[Build Silver Layer] Clean & Load.sql
+++ sql-data-warehouse-project/scripts/silver/[Build Silver Layer] Clean & Load.sql
@@ -108,8 +108,23 @@ SELECT
 	END AS sls_price 
 FROM bronze.crm_sales_details
 
+-- [Build Silver Layer] Clean & Load erp_cust_az12
+	
+INSERT INTO silver.erp_cust_az12(cid, bdate, gen)
 
-
+SELECT 
+CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LEN(cid)) --Remove 'NAS' prefix if present 
+	ELSE cid 
+END cid, 
+CASE WHEN bdate > GETDATE() THEN NULL
+	ELSE bdate
+END AS bdate, -- Set future birthdates to NULL 
+CASE 
+	WHEN UPPER(TRIM(gen)) IN ('F', 'Female') THEN 'Female'
+	WHEN UPPER(TRIM(gen)) IN ('M', 'Male') THEN 'Male'
+	ELSE 'n/a'
+END AS gen -- Normalize gender values and handle unknown cases 
+FROM bronze.erp_cust_az12
  
 
 
